a3055309fc1795341da92aaacb8f5276f8b22c55
Anand M <anandmehrotratr@gmail.com>
2023-08-27 10:49:53 -0500

    Fixing Beads AND Adding VSCODE Settings

--- usaco/beads/beads.java
+++ usaco/beads/beads.java
@@ -7,24 +7,17 @@ import java.io.*;
 import java.util.*;
 
 class beads {
-
-
+  private static final String BASE_PATH = "/Users/anandmehrotra/Documents/Interview/competitive_programming/usaco/beads/";
   public static void main (String [] args) throws IOException {
     // Use BufferedReader rather than RandomAccessFile; it's much faster
-    BufferedReader f = new BufferedReader(new FileReader("friday.in"));
+    BufferedReader f = new BufferedReader(new FileReader(BASE_PATH + "beads.in"));
                                                   // input file name goes above
-    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("friday.out")));
+    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter( BASE_PATH + "beads.out")));
     // Use StringTokenizer vs. readLine/split -- lots faster
     StringTokenizer st = new StringTokenizer(f.readLine());
-    friday friday = new friday();
-  
-    Map<Day, Integer> ans = friday.compute(Integer.parseInt(st.nextToken()));
-    StringBuilder str = new StringBuilder();
-    for(Day day : Day.values()) {
-      str.append(ans.get(day));
-      str.append(" ");
-    }
-    out.println(str.length() > 0 ? str.substring(0, str.length() - 1) : "");  
+    int val = Integer.parseInt(st.nextToken());
+    beads beads = new beads();
+    out.println(val);  
     out.close();                                  // close the output file
   }
 }
